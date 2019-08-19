bc06f50e3e0bf7f47d9f53fe7876131d317248d9
braisedPanda <987433278@qq.com>
2019-08-19 21:14:44 +0800

    测试上传

--- src/main/java/com/braisedpanda/shirotest/Utils/Utils.java
+++ src/main/java/com/braisedpanda/shirotest/Utils/Utils.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Utils {
-    //获取一个数组的最大值
+    //获取一个数组的最大值666
     public double getMax(List<Double> list){
         if(list!=null && list.size()>0) {
             double max = list.get(0);
